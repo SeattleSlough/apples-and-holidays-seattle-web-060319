36c8486ab6ff1e29252874aaa39b38a7e71d6eaf
Michael Maggs <michaelmaggs@gmail.com>
2019-05-21 02:00:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |supplies, item|
       if item == "BBQ"
-        new_array.push(holiday)
+        new_array.push(:"holiday")
       end
     end
   end
